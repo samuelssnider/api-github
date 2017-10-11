9c9df995b257ed947956e38d2b841719c639fadc
samuelssnider <sam_redins@yahoo.com>
2017-10-11 16:42:31 -0600

    working on service for all the things

---
+++ app/services/at_your_service.rb
@@ -0,0 +1,9 @@
+class AtYourService
+	
+	def initialize(user)
+		@conn = Faraday.new(url: "https://github.com") do |faraday|
+			faraday.token_auth("#{user.oauth_token}")
+      faraday.headers["X-API-Key"] = ENV["github_key"]
+			faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+		end
+end
