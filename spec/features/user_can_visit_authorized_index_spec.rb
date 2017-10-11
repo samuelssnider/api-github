c022c0830b4e1d175094188a6313c4831e831217
samuelssnider <sam_redins@yahoo.com>
2017-10-11 17:39:12 -0600

    added database cleaner

--- spec/features/user_can_visit_authorized_index_spec.rb
+++ spec/features/user_can_visit_authorized_index_spec.rb
@@ -1,3 +1,11 @@
 require 'rails'
 
-describe 'as a guest'
+describe 'as a guest i can login as a user using github' do
+	it 'and see the authorized index page' do
+		include Helper
+		create_a_user
+		binding.pry
+		
+	end
+end
+		
