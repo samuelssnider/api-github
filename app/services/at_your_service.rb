e94aadc9bfad340e8cdf9e7faa965552c4faedcf
samuelssnider <sam_redins@yahoo.com>
2017-10-14 14:06:05 -0600

    last one

--- app/services/at_your_service.rb
+++ app/services/at_your_service.rb
@@ -5,7 +5,7 @@ class AtYourService
 		@user = user
 		@conn = Faraday.new(url: "https://api.github.com") do |faraday|
 			# faraday.token_auth("#{user.oauth_token}")
-      # faraday.headers["X-API-Key"] = ENV["github_key"]
+      faraday.headers["X-API-Key"] = ENV["github_key"]
 			faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
 		end
 	end
@@ -34,6 +34,9 @@ class AtYourService
 	def following
 		parse_url("users/#{user.user_name}/following").map{|user| [user["login"], user["html_url"]]}
 	end
+	
+	def my_events
+		parse_url
 	
 	def self.set_user(uid)
 		@user = User.find_by(uid: uid)
